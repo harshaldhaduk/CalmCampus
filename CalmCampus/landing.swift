f44a36d31cfdbd9b4a14b4789afabbf5008797a0
harshaldhaduk <harshaldhaduk123@gmail.comwhas>
2023-10-08 01:11:21 -0500

    firebase integration + welcome -> redone to landing

---
+++ CalmCampus/landing.swift
@@ -0,0 +1,18 @@
+//
+//  landing.swift
+//  CalmCampus
+//
+//  Created by Harshal Dhaduk on 10/7/23.
+//
+
+import SwiftUI
+
+struct landing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    landing()
+}
